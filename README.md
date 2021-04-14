286edddabd0e3109903f6132259fb065559a8870
Ljzkirito <Kirito.ljz@outlook.com>
2021-04-14 20:56:01 +0800

    test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Github Actions Padavan RM2100
+
+- Padavan源码是[hanwckf/rt-n56u](https://github.com/hanwckf/rt-n56u)。
+- Github Actions参考自[P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)&[hanwckf/scut_padavan_build](https://github.com/hanwckf/scut_padavan_build)。
+- 编译目标为Redmi-AC2100
---
+++ diy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy.sh
+# Description: OpenWrt DIY script (After git clone)
+#
+
+# 修改Padavan登陆地址
+sed -i 's/192.168.2.1/192.168.5.1/g' trunk/user/shared/defaults.h
+sed -i 's/192.168.2.100/192.168.5.50/g' trunk/user/shared/defaults.h
+sed -i 's/192.168.2.244/192.168.5.100/g' trunk/user/shared/defaults.h
