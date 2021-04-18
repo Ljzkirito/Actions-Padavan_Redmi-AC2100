ebe0bf2c4531512f954aafe50fa25ea30cd71c75
Ljzkirito <kirito.ljz@gmail.com>
2021-04-18 20:21:23 +0800

    fix

--- diy.sh
+++ diy.sh
@@ -11,6 +11,6 @@
 #
 
 # 修改Padavan登陆地址
-sed -i 's/192.168.2.1/192.168.5.1/g' trunk/user/shared/defaults.h
-sed -i 's/192.168.2.100/192.168.5.50/g' trunk/user/shared/defaults.h
-sed -i 's/192.168.2.244/192.168.5.100/g' trunk/user/shared/defaults.h
+sed -i 's/192.168.2.1"/192.168.5.1"/' trunk/user/shared/defaults.h
+sed -i 's/192.168.2.100/192.168.5.50/' trunk/user/shared/defaults.h
+sed -i 's/192.168.2.244/192.168.5.100/' trunk/user/shared/defaults.h
